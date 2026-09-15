197bb8bd72b1830755578ba8ae74a9939f57ae3f
Alexander Stach <stach.alex@yahoo.de>
2026-09-15 08:23:21 +0200

    Initial Commit #2

---
+++ Controllers/NexoraController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nexora.Api.Services;
+
+namespace Nexora.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NexoraController : ControllerBase
+    {
+        [HttpGet]
+        public string GetNexoraStatus()
+        {
+            return "Nexora API is running.";
+        }
+
+    }
+}
---
+++ Services/NexoraService.cs
@@ -0,0 +1,10 @@
+﻿namespace Nexora.Api.Services
+{
+    public class NexoraService
+    {
+        public string GetNexoraMessage()
+        {
+            return "Nexora API is running.";
+        }
+    }
+}
